5fa925fd654dead0662afb7ce90580d148a2d9e6
Irene María <Irene Mara@LAPTOP-AANKOAAJ>
2020-10-30 16:57:45 +0000

    Segunda Versión de Kata3

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,8 +3,18 @@ package kata3;
 public class Kata3 {
 
 public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay("HISTOGRAM");
-        histo.execute();
-    }
-
+        Histogram<String> histogram = new Histogram();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM", histogram);
+        histogramDisplay.execute();
+    } 
 }
